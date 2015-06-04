6f6c6c9e7b64c1c7150395b8f56fe0846cc89b38
Nessa Stewart <penessa@gmail.com>
2015-06-04 12:44:09 -0500

    fixing datatbase stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509203315) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "people", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "birthdate"
-  end
 
 end
